30be774deb0da745aebb8a9bbb7cf45bebda7cf3
ADRIX <aadityasahori@gmail.com>
2025-09-08 18:46:34 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,13 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDePtfAKouNDGoNY27_mIfSG096hRJ9U1U",
+  authDomain: "adrix-1.firebaseapp.com",
+  projectId: "adrix-1",
+  storageBucket: "adrix-1.firebasestorage.app",
+  messagingSenderId: "633957470456",
+  appId: "1:633957470456:web:0865dfe89f5e9f6df2ab0d"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
