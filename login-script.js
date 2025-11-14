50f26a67e2a0a66bb0e7c217a358a1d33780b60b
ADRIX <aadityasahori@gmail.com>
2025-11-14 17:34:18 +0530

    Add files via upload

---
+++ login-script.js
@@ -0,0 +1,4 @@
+function showFrom(formId) {
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"))
+    document.getElementById(formId).classList.add("active");
+}
