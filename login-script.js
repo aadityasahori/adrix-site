da0623642bdfe1280f79f634f3448a40f32140ca
ADRIX <aadityasahori@gmail.com>
2026-04-05 14:21:28 +0530

    Delete login-script.js

--- login-script.js
+++
@@ -1,4 +0,0 @@
-function showFrom(formId) {
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"))
-    document.getElementById(formId).classList.add("active");
-}
